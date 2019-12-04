5803ac10b8803e1c45e96e7f36c1ff04641a1053
hwjang <kirnot92@gmail.com>
2019-12-04 12:44:26 +0900

    dictionary 가져옴

---
+++ src/scripts/collection/dictionary.ts
@@ -0,0 +1,71 @@
+
+export default class Dictionary<TKey, TValue>
+{
+    _map: Map<TKey,TValue> = new Map<TKey,  TValue>();
+
+    constructor(init?: { key: TKey; value: TValue; }[])
+    {
+        if (init === undefined)
+        {
+            return
+        }
+
+        for (const { key, value } of init)
+        {
+            this.Add(key, value);
+        }
+    }
+
+    public Set(key: TKey, value: TValue)
+    {
+        this._map.set(key, value);
+    }
+
+    public Add(key: TKey, value: TValue)
+    {
+        if (this.ContainsKey(key))
+        {
+            throw new Error("키 중복");
+        }
+        this.Set(key, value);
+    }
+
+    public Remove(key: TKey): boolean
+    {
+        if (this.ContainsKey(key))
+        {
+            this._map.delete(key);
+            return true;
+        }
+        return false;
+    }
+
+    public Keys(): Array<TKey>
+    {
+        return Array.from(this._map.keys());
+    }
+
+    public Values(): Array<TValue>
+    {
+        return Array.from(this._map.values());
+    }
+
+    public ContainsKey(key: TKey): boolean
+    {
+        return this._map.has(key);
+    }
+
+    public TryGet(key: TKey): TValue
+    {
+        if (this.ContainsKey(key))
+        {
+            return this.MustGet(key);
+        }
+        return null;
+    }
+
+    public MustGet(key: TKey): TValue
+    {
+        return this._map.get(key);
+    }
+}
