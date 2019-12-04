98b02c426d5f692df7194543fa9037efebda0835
hwjang <kirnot92@gmail.com>
2019-12-04 13:10:01 +0900

    List<T> 구현

---
+++ src/scripts/collection/list.ts
@@ -0,0 +1,36 @@
+
+export default class List<T>
+{
+    arr: Array<T> = new Array<T>();
+
+    public Add(value: T)
+    {
+        this.arr.push(value);
+    }
+
+    public Remove(value: T)
+    {
+        var index = this.IndexOf(value);
+        if (index != -1)
+        {
+            this.arr.splice(index, 1);
+        }
+    }
+
+    public Contains(value: T): boolean
+    {
+        return this.IndexOf(value) >= 0;
+    }
+
+    IndexOf(value: T): number
+    {
+        for(var i = 0; i < this.arr.length; ++i)
+        {
+            if (this.arr[i] ==  value)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
